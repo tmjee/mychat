a55209d1973b76007f7ea192e37fc2956bec1ef8
tmjee <tmjee1@gmail.com>
2015-06-16 21:46:57 +1000

    initial commit

--- server/src/main/java/com/tmjee/mychat/service/LogonServices.java
+++ server/src/main/java/com/tmjee/mychat/service/LogonServices.java
@@ -3,6 +3,8 @@ package com.tmjee.mychat.service;
 import com.google.inject.Inject;
 import com.tmjee.jooq.generated.tables.MychatUser;
 import com.tmjee.mychat.rest.Logon;
+import org.jooq.Record;
+import org.jooq.Result;
 import org.jooq.impl.DSL;
 
 import static org.jooq.impl.DSL.*;
@@ -24,10 +26,18 @@ public class LogonServices {
 
 
     public void logon(String email, String password) {
-        select(count())
+        Result<Record> result =
+            select()
                 .from(MYCHAT_USER)
-                .where(MYCHAT_USER.IDENTIFICATION_TYPE).equals(IdentificationTypeEnum.EMAIL)
-                ;
+                .where(MYCHAT_USER.IDENTIFICATION_TYPE.eq(IdentificationTypeEnum.EMAIL.name()))
+                .and(MYCHAT_USER.IDENTIFICATION_TYPE.isNotNull())
+                .and(MYCHAT_USER.IDENTIFICATION.isNotNull())
+                .and(MYCHAT_USER.IDENTIFICATION.eq(email))
+                .fetch();
+        if (result.isNotEmpty()) {
+            Record record = result.get(0);
+            record.getValue(MYCHAT_USER.SALT);
+        }
     }
 
 
