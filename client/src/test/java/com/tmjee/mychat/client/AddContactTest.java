0b8475a1af769ea23197fba5aee9f48201af7628
tmjee <tmjee1@gmail.com>
2015-07-12 23:53:53 +1000

    initial commit

--- client/src/test/java/com/tmjee/mychat/client/AddContactTest.java
+++ client/src/test/java/com/tmjee/mychat/client/AddContactTest.java
@@ -1,7 +1,32 @@
 package com.tmjee.mychat.client;
 
+import com.tmjee.mychat.common.domain.GenderEnum;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.Map;
+import java.util.UUID;
+
+import static com.tmjee.mychat.client.Static.*;
+
 /**
  * @author tmjee
  */
 public class AddContactTest {
+
+    @Test
+    public void testAddContact() throws IOException, URISyntaxException {
+        String u1 = UUID.randomUUID().toString();
+
+        Map<String, Object> m = MYCHAT_CLIENT.register(u1+"@gmail.com", "test", u1, GenderEnum.FEMALE);
+
+        String activationToken = m.get("activationToken").toString();
+        Integer registeredMyChatUserId = (Integer) m.get("myChatUserId");
+
+        MYCHAT_CLIENT.activate(activationToken);
+
+        MYCHAT_CLIENT.logon("toby@gmail.com", "test");
+        MYCHAT_CLIENT.addContact(registeredMyChatUserId);
+    }
 }
