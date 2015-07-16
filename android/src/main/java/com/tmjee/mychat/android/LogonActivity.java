c2069ff92e6ed368d4a7773367422795559ea97f
tmjee <tmjee1@gmail.com>
2015-07-16 17:55:43 +1000

    initial commit

---
+++ android/src/main/java/com/tmjee/mychat/android/LogonActivity.java
@@ -0,0 +1,37 @@
+package com.tmjee.mychat.android;
+
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import java.nio.channels.AsynchronousCloseException;
+
+/**
+ * @author tmjee
+ */
+public class LogonActivity extends Activity {
+
+    public static final String INTENT_EXTRA_ORIGINAL_ACTIVITY = "INTENT_EXTRA_ORIGINAL_ACTIVITY";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getIntent().getStringExtra(INTENT_EXTRA_ORIGINAL_ACTIVITY);
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+
+    private static class LogonAsyncTask extends AsyncTask<Bundle, Integer, Bundle> {
+
+        @Override
+        protected Bundle doInBackground(Bundle... params) {
+            return null;
+        }
+    }
+}
