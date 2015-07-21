a9ca26a223395ed887d978e54675e939a9f14b52
tmjee <tmjee1@gmail.com>
2015-07-21 18:24:03 +1000

    initial commit

--- android/src/com/tmjee/mychat/android/MainActivity.java
+++ android/src/com/tmjee/mychat/android/MainActivity.java
@@ -1,5 +1,7 @@
 package com.tmjee.mychat.android;
 
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 
 public class MainActivity extends AbstractAuthenticationActivity {
@@ -9,6 +11,47 @@ public class MainActivity extends AbstractAuthenticationActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        InternalTabListener tabListener = new InternalTabListener();
+
+        ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        actionBar.addTab(
+                actionBar.newTab()
+                    .setText("Chats")
+                    .setTabListener(tabListener));
+        actionBar.addTab(
+                actionBar.newTab()
+                    .setText("Contacts")
+                    .setTabListener(tabListener));
+        actionBar.addTab(
+                actionBar.newTab()
+                    .setText("Discover")
+                    .setTabListener(tabListener));
+        actionBar.addTab(
+                actionBar.newTab()
+                    .setText("Myself")
+                    .setTabListener(tabListener));
         setContentView(R.layout.main);
+    }
+
+
+
+    private static class InternalTabListener implements ActionBar.TabListener {
+
+        @Override
+        public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+            fragmentTransaction.replace()
+        }
+
+        @Override
+        public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
+        }
+
+        @Override
+        public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
+
+        }
     }
 }
